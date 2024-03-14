fdbeab533d3dec009d7885b1dffa9eea4bd0583c
Kiril <kiril2315@gmail.com>
2024-03-14 21:11:38 +0200

    use tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {/* on search items-center becomes items-start */}
+      <div className="flex justify-center items-center h-screen">
+        <input type="text" placeholder="Search Pinterest" className="w-full sm:w-3/12 border border-gray-300 px-4 py-2 rounded-3xl focus:border-blue-500" />
+        <button className="bg-blue-400 text-white px-4 py-2 rounded-3xl ml-2">Search</button>
+      </div>
     </>
   )
 }
