c4161ac6db6290e32b61085510c78f018c20334d
Kiril <kiril2315@gmail.com>
2024-03-15 10:16:42 +0200

    setup axios

--- api/pinapi/pinapi/settings.py
+++ api/pinapi/pinapi/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,8 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:4173"
 ]
 
 ROOT_URLCONF = 'pinapi.urls'
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pins, setPins] = useState([])
 
+  const handleNewSearch = async () => {
+
+    const res = await axios.get('BaseSearchResource', {
+      params: { 
+        q: 'cats'
+      }
+    })
+
+    setPins([res])
+
+    console.log(res)
+  }
 
   return (
     <>
       {/* on search items-center becomes items-start */}
       <div className="flex justify-center items-center h-screen">
         <input type="text" placeholder="Search Pinterest" className="w-full sm:w-3/12 border border-gray-300 px-4 py-2 rounded-3xl focus:border-blue-500" />
-        <button className="bg-blue-400 text-white px-4 py-2 rounded-3xl ml-2">Search</button>
+        <button onClick={handleNewSearch} className="bg-blue-400 text-white px-4 py-2 rounded-3xl ml-2">Search</button>
+      </div>
+      <div>
+        {pins.map((pin) => {
+          <div>{pin}</div>
+        })}
       </div>
     </>
   )
