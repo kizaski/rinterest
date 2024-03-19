7ff7175503912b5d1be501b2d9cfbc4c6cb3463d
Kiril <kiril2315@gmail.com>
2024-03-19 21:52:03 +0200

    use vite env file for base url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
